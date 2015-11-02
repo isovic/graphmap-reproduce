b55745126c42e9254b6a203c54818ebefea2104c
Ivan Sovic (T530) <ivan.sovic@gmail.com>
2015-11-02 18:03:19 +0100

    Working on automatic setup of the data.

--- src/run.py
+++ src/run.py
@@ -67,19 +67,71 @@ def setup_data():
 	# archive_basename = 'Ecoli_R7_NONI'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/downloads/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
 	# archive_basename = 'Ecoli_R7_ONI_flowcell_18'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/ >> %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
 	# archive_basename = 'Ecoli_R7_ONI_flowcell_17'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/ >> %s'  % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	# execute_command('mkdir -p %s; cp %s/../data/downloads/%s/%s %s/'  % (reads_folder, SCRIPT_PATH, folder_name, fastq_file, reads_folder));
+
+	# folder_name = 'R7.3';
+	# fastq_file = 'ecoliR7.3.fastq';
+	# reads_folder = '%s/../data/consensus-ecoliR7.3/reads/' % (SCRIPT_PATH);
+	# archive_basename = 'Ecoli_R73'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/downloads/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	# execute_command('mkdir -p %s; cp %s/../data/downloads/%s/%s %s/'  % (reads_folder, SCRIPT_PATH, folder_name, fastq_file, reads_folder));
+
+	# folder_name = 'ecoli-uti89';
+	# fastq_file = 'reads_ecoli_uti89.fastq';
+	# reads_folder = '%s/../data/consensus-uti89/reads/' % (SCRIPT_PATH);
+	# archive_basename = 'reads'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/fast5/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	# execute_command('mkdir -p %s; cp %s/../data/downloads/%s/%s %s/'  % (reads_folder, SCRIPT_PATH, folder_name, fastq_file, reads_folder));
+
+	folder_name = 'salmonella-typhi';
+	fastq_file = 'reads-typhi.fastq';
+	reads_folder = '%s/../data/consensus-salmonellatyphi/reads/' % (SCRIPT_PATH);
+	archive_basename = 'H566_ON_inc'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/H566_ON_inc/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'H566_30_min_inc'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/H566_30_min_inc/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'raw_2_rabsch_R7'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/raw_2_rabsch_R7/downloads/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
 	execute_command('mkdir -p %s; cp %s/../data/downloads/%s/%s %s/'  % (reads_folder, SCRIPT_PATH, folder_name, fastq_file, reads_folder));
 
-	folder_name = 'R7.3';
-	fastq_file = 'ecoliR7.3.fastq';
-	reads_folder = '%s/../data/consensus-ecoliR7.3/reads/' % (SCRIPT_PATH);
-	archive_basename = 'Ecoli_R73'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/downloads/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	folder_name = 'amplicons';
+	fastq_file = 'reads_all-f1000.fastq';
+	reads_folder = '%s/../data/amplicons-f1000-1d2d/reads/' % (SCRIPT_PATH);
+	archive_basename = 'SRR1747434'; execute_command('cd %s/../data/downloads/%s; cat %s.fastq > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'SRR1748410'; execute_command('cd %s/../data/downloads/%s; cat %s.fastq >> %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
 	execute_command('mkdir -p %s; cp %s/../data/downloads/%s/%s %s/'  % (reads_folder, SCRIPT_PATH, folder_name, fastq_file, reads_folder));
 
-	folder_name = 'ecoli-uti89';
-	fastq_file = 'reads_ecoli_uti89.fastq';
-	reads_folder = '%s/../data/consensus-uti89/reads/' % (SCRIPT_PATH);
-	archive_basename = 'reads'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/fast5/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	# reads_folder = '%s/../data/amplicons-f1000/reads/' % (SCRIPT_PATH);
+	# execute_command('%s/fastqfilter.py %s/../data/downloads/%s/reads_all-f1000.fastq %s/../data/downloads/%s/reads_2d.fastq'  % (SAMSCRIPTS, SCRIPT_PATH, folder_name, SCRIPT_PATH, folder_name));
+	
+
+	folder_name = 'be1';
+	fastq_file = 'reads-BE1.fastq';
+	reads_folder = '%s/../data/consensus-BE1/reads/' % (SCRIPT_PATH);
+	archive_basename = 'FAA37759_GB2974_MAP005_20150423__2D_basecalling_v1.14_2D'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
 	execute_command('mkdir -p %s; cp %s/../data/downloads/%s/%s %s/'  % (reads_folder, SCRIPT_PATH, folder_name, fastq_file, reads_folder));
+
+	folder_name = 'adp1';
+	fastq_file = 'all_1d2d.fastq';
+	reads_folder = '%s/../data/consensus-ADP1/reads/' % (SCRIPT_PATH);
+	archive_basename = 'AWK_ONT_MN2064525'; execute_command('cd %s/../data/downloads/%s; cat %s.fastq > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'AWK_ONT_MN2064006'; execute_command('cd %s/../data/downloads/%s; cat %s.fastq >> %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'AWK_ONT_FAA43210'; execute_command('cd %s/../data/downloads/%s; cat %s.fastq >> %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'AWK_ONT_FAA43204'; execute_command('cd %s/../data/downloads/%s; cat %s.fastq >> %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'AWK_ONT_FAA17573'; execute_command('cd %s/../data/downloads/%s; cat %s.fastq >> %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	execute_command('mkdir -p %s; cp %s/../data/downloads/%s/%s %s/'  % (reads_folder, SCRIPT_PATH, folder_name, fastq_file, reads_folder));
+
+	folder_name = 'ecoli-nmeth';
+	fastq_file = 'reads-nmeth-all_2d.fastq';
+	reads_folder = '%s/../data/consensus-ecolinmeth/reads/' % (SCRIPT_PATH);
+	archive_basename = 'flowcell_20_LomanLabz_PC_Ecoli_K12_R7.3'; execute_command('cd %s/../data/downloads/%s; poretools --type 2D fastq %s/flowcell_20/1.9/downloads/pass/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'flowcell_32_LomanLabz_K12_His_tag'; execute_command('cd %s/../data/downloads/%s; poretools --type 2D fastq %s/flowcell_32/downloads/pass/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'flowcell_33_LomanLabz_PC_K12_0.4SPRI_Histag'; execute_command('cd %s/../data/downloads/%s; poretools --type 2D fastq %s/flowcell_33/downloads/pass/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'flowcell_39'; execute_command('cd %s/../data/downloads/%s; poretools --type 2D fastq %s/flowcell_39_K12_Histag/downloads/pass/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	execute_command('mkdir -p %s; cp %s/../data/downloads/%s/%s %s/'  % (reads_folder, SCRIPT_PATH, folder_name, fastq_file, reads_folder));
+
+	folder_name = 'ecoli-map006';
+	fastq_file = 'reads-MAP006-1.fastq';
+	reads_folder = '%s/../data/consensus-ecoliMAP006/reads/' % (SCRIPT_PATH);
+	archive_basename = 'MAP006-1.basecalled'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/MAP006-1_downloads/pass/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	archive_basename = 'MAP006-1.basecalled'; execute_command('cd %s/../data/downloads/%s; poretools fastq %s/MAP006-1_downloads/fail/ > %s' % (SCRIPT_PATH, folder_name, archive_basename, fastq_file));
+	execute_command('mkdir -p %s; cp %s/../data/downloads/%s/%s %s/'  % (reads_folder, SCRIPT_PATH, folder_name, fastq_file, reads_folder));
+
 	return;
 
 	sys.stderr.write('\tExtracting the R7 dataset.\n');
@@ -118,6 +170,8 @@ def setup_data():
 	archive_basename = 'AWK_ONT_FAA43210';			execute_command('cd %s/../data/downloads/; cd %s; gunzip --keep %s.fastq.gz' % (SCRIPT_PATH, folder_name, archive_basename));
 	archive_basename = 'AWK_ONT_FAA43204';			execute_command('cd %s/../data/downloads/; cd %s; gunzip --keep %s.fastq.gz' % (SCRIPT_PATH, folder_name, archive_basename));
 	archive_basename = 'AWK_ONT_FAA17573';			execute_command('cd %s/../data/downloads/; cd %s; gunzip --keep %s.fastq.gz' % (SCRIPT_PATH, folder_name, archive_basename));
+
+#####
 	
 	sys.stderr.write('\tExtracting the Loman et al. dataset used for nanopore-only assembly.\n');
 	folder_name = 'ecoli-nmeth';
